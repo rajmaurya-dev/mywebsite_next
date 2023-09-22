151a9606046c57536ebc4fc5414edcf532132dd1
rohit-sama <rohit9804singh@gmail.com>
2023-09-23 00:19:41 +0530

    design changes 2.0

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 interface pageProps {}
@@ -9,15 +10,45 @@ const page: React.FC<pageProps> = () => {
 
       <div className="text-left lg:border-l-1 lg:pl-3 text-gray-400 gap-5 text-md flex-col flex ">
         -- learning right now --
-        <div className="">
+        
+        <div className="border-b-1 border-dashed pb-2">
           <h4 className="decoration-gray-400 text-gray-300 underline-offset-4 underline">
             {" "}
-            Present (July - August) 2023{" "}
+            [Present] September, 2023{" "}
           </h4>
           <h2 className="colorchange">
             {" "}
-            learning Deep Learning && developing website with Next.js
+            Delving Deep into web Technologies and exploring some 
+             opportunities.
           </h2>
+          <div>
+            <button className="border-1  hover:bg-slate-700 border-slate-700 rounded-sm px-1 mr-2 mt-1">
+              Next.js
+            </button>
+            <button className="border-1 hover:bg-slate-700 border-slate-700 rounded-sm px-1 mr-2 mt-1">
+             Data Structures and Algorithms
+            </button>
+          </div>
+        </div>
+        
+        <div className="">
+          <h4 className="decoration-gray-400 text-gray-300 underline-offset-4 underline">
+            {" "}
+             July - August (2023){" "}
+          </h4>
+          <h2 className="colorchange">
+            {" "}
+            Exploring machine learning while developing Next.js
+             Applications
+          </h2>
+          <div>
+            <button className="border-1  hover:bg-slate-700 border-slate-700 rounded-sm px-1 mr-2 mt-1">
+              Next.js
+            </button>
+            <button className="border-1 hover:bg-slate-700 border-slate-700 rounded-sm px-1 mr-2 mt-1">
+              <Link href="https://www.fast.ai/" >Machine learning </Link>
+            </button>
+          </div>
         </div>
         <div className="">
           <h4 className="decoration-gray-400 text-gray-300 underline-offset-4 underline">
@@ -25,8 +56,14 @@ const page: React.FC<pageProps> = () => {
             June 2023{" "}
           </h4>
           <h2 className="colorchange">
-            learned Android (Flutter) development, was exploring stuff
+            learned Android (Flutter) development, was just exploring stuff
           </h2>
+          <div>
+          
+            <button className="border-1 hover:bg-slate-700  border-slate-700 rounded-sm px-1 mr-2 mt-1">
+            Flutter
+            </button>
+          </div>
         </div>
         <div className="">
           <h4 className="decoration-gray-400 text-gray-300 underline-offset-4 underline">
@@ -35,23 +72,53 @@ const page: React.FC<pageProps> = () => {
           </h4>
           <h2 className="colorchange">
             learned Frontend and Backend development through free resources
-            <br />
-            on the internet.
+            <br/> on the  internet.
           </h2>
+          <div>
+            <button className="border-1  hover:bg-slate-700 border-slate-700 rounded-sm px-1 mr-2 mt-1">
+              Node.js
+            </button>
+            <button className="border-1 hover:bg-slate-700 border-slate-700 rounded-sm px-1 mr-2 mt-1">
+              Database
+            </button>
+            <button className="border-1 hover:bg-slate-700 border-slate-700 rounded-sm px-1 mr-2 mt-1">
+              RestApi
+            </button>
+          </div>
         </div>
         <div className="">
           <h4 className="decoration-gray-400 text-gray-300 underline-offset-4 underline">
             {" "}
             MARCH - APRIL (2023){" "}
           </h4>
-          <h2 className="colorchange">React.js Developer</h2>
+          <h2 className="colorchange">learned React.js and various other frontend
+          <br /> technologies</h2>
+          <div>
+            <button className="border-1 hover:bg-slate-700  border-slate-700 rounded-sm px-1 mr-2 mt-1">
+              React.js
+            </button>
+            <button className="border-1 hover:bg-slate-700 border-slate-700 rounded-sm px-1 mr-2 mt-1">
+              Tailwindcss
+            </button>
+          </div>
         </div>
         <div className="">
           <h4 className="decoration-gray-400 text-gray-300 underline-offset-4 underline">
             {" "}
             January - March (2023){" "}
           </h4>
-          <h2 className="">Front-End Developer</h2>
+          <h2 className="">Learned Front-End Development</h2>
+          <div>
+            <button className="border-1 hover:bg-slate-700  border-slate-700 rounded-sm px-1 mr-2 mt-1">
+              Javascript
+            </button>
+            <button className="border-1 hover:bg-slate-700 border-slate-700 rounded-sm px-1 mr-2 mt-1">
+              Java
+            </button>
+            <button className="border-1 hover:bg-slate-700 border-slate-700 rounded-sm px-1 mr-2 mt-1">
+              Basic webdev not worth mentioning
+            </button>
+          </div>
         </div>
       </div>
     </div>
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -9,7 +9,8 @@ const page: React.FC<pageProps> = () => {
       <div className="text-left lg:border-l-1 lg:pl-3 text-gray-400 gap-5 text-md flex-col flex ">
         <div className="">
           <h2 className="decoration-gray-400 text-gray-300 underline-offset-4 underline">
-            <a className=""
+            <a
+              className=""
               href="https://github.com/rohit-sama/next-chat"
               rel="noreferrer"
               target="_blank"
@@ -22,6 +23,49 @@ const page: React.FC<pageProps> = () => {
             <br />
             Powered by NextAuth!
           </p>
+          <div>
+            <button className="border-1 border-slate-700 rounded-sm px-1 mr-2 mt-1">Next.js</button>
+            <button className="border-1 border-slate-700 rounded-sm px-1  mr-2 mt-1">
+              Pusher.js
+            </button>
+            <button className="border-1 border-slate-700 rounded-sm px-1   mr-2 mt-1">
+              NextAuth
+            </button>
+            <button className="border-1 border-slate-700 rounded-sm px-1  mr-2 mt-1">
+              ReddisDb
+            </button>
+            <button className="border-1 border-slate-700 rounded-sm px-1  mr-2 mt-1">
+              Tailwindcss
+            </button>
+          </div>
+        </div>
+        <div className="">
+          <h2 className="decoration-gray-400 text-gray-300 underline-offset-4 underline">
+            <a
+              href="https://github.com/rohit-sama/QouteShare"
+              rel="noreferrer"
+              target="_blank"
+            >
+              QOUTESHARE
+            </a>
+          </h2>
+          <p className="">
+            designed for folks who are interested in sharing motivational quotes
+            <br />
+            to inspire and support each other.
+          </p>
+          <div>
+            <button className="border-1 border-slate-700 rounded-sm px-1 mr-2 mt-1">Next.js</button>
+            <button className="border-1 border-slate-700 rounded-sm px-1   mr-2 mt-1">
+              NextAuth
+            </button>
+            <button className="border-1 border-slate-700 rounded-sm px-1  mr-2 mt-1">
+              MongoDb
+            </button>
+            <button className="border-1 border-slate-700 rounded-sm px-1  mr-2 mt-1">
+              Tailwindcss
+            </button>
+          </div>
         </div>
 
         <div className="">
@@ -39,40 +83,52 @@ const page: React.FC<pageProps> = () => {
             <br />
             seeking unique travel destinations that resonate with their passions
           </p>
+          <div>
+            <button className="border-1 border-slate-700 rounded-sm px-1 mr-2 mt-1">
+              React.js
+            </button>
+            <button className="border-1 border-slate-700 rounded-sm px-1   mr-2 mt-1">
+              JwtAuth
+            </button>
+            <button className="border-1 border-slate-700 rounded-sm px-1  mr-2 mt-1">
+              MongoDb
+            </button>
+            <button className="border-1 border-slate-700 rounded-sm px-1  mr-2 mt-1">
+              Tailwindcss
+            </button>
+          </div>
         </div>
-
         <div className="">
           <h2 className="decoration-gray-400 text-gray-300 underline-offset-4 underline">
             <a
-              href="https://github.com/rohit-sama/QouteShare"
+              href="https://github.com/rohit-sama/backend_project_todo"
               rel="noreferrer"
               target="_blank"
             >
-              QOUTESHARE
+              TO-DO APP
             </a>
           </h2>
           <p className="">
-            designed for folks who are interested in sharing motivational quotes
+            This is a ONLY TODO api you will ever need, made with nodejs on
+            backend and
             <br />
-            to inspire and support each other.
+            MongoDB for the database and hosted on vercel.
           </p>
-        </div>
-
-        <div className="">
-          <h2 className="decoration-gray-400 text-gray-300 underline-offset-4 underline">
-            <a
-              href="https://github.com/rohit-sama/movie-app_react"
-              rel="noreferrer"
-              target="_blank"
-            >
-              MOVIE-APP
-            </a>
-          </h2>
-          <p className="">
-            This is a basic movie app where you can search your favourite movies
-            <br />
-            which works by using imdb api and made with react.js
-          </p>
+          <div>
+            <button className="border-1 border-slate-700 rounded-sm px-1 mr-2 mt-1">Node.js</button>
+            <button className="border-1 border-slate-700 rounded-sm px-1  mr-2 mt-1">
+              Express.js
+            </button>
+            <button className="border-1 border-slate-700 rounded-sm px-1   mr-2 mt-1">
+              NextAuth
+            </button>
+            <button className="border-1 border-slate-700 rounded-sm px-1  mr-2 mt-1">
+              ReddisDb
+            </button>
+            <button className="border-1 border-slate-700 rounded-sm px-1  mr-2 mt-1">
+              Tailwindcss
+            </button>
+          </div>
         </div>
 
         <div className="">
@@ -90,24 +146,36 @@ const page: React.FC<pageProps> = () => {
             <br />
             in march 2023.
           </p>
+          <div>
+            <button className="border-1 border-slate-700 rounded-sm px-1 mr-2 mt-1">
+              React.js
+            </button>
+            <button className="border-1 border-slate-700 rounded-sm px-1  mr-2 mt-1">
+              Tailwindcss
+            </button>
+          </div>
         </div>
-
         <div className="">
           <h2 className="decoration-gray-400 text-gray-300 underline-offset-4 underline">
             <a
-              href="https://github.com/rohit-sama/backend_project_todo"
+              href="https://github.com/rohit-sama/movie-app_react"
               rel="noreferrer"
               target="_blank"
             >
-              TO-DO APP
+              MOVIE-APP
             </a>
           </h2>
           <p className="">
-            This is a ONLY TODO api you will ever need, made with nodejs on
-            backend and
+            This is a basic movie app where you can search your favourite movies
             <br />
-            MongoDB for the database and hosted on vercel.
+            which works by using imdb api and made with react.js
           </p>
+          <div>
+            <button className="border-1 border-slate-700 rounded-sm px-1 mr-2 mt-1">
+              React.js
+            </button>
+            <button className="border-1 border-slate-700 rounded-sm px-1  mr-2 mt-1">Api's</button>
+          </div>
         </div>
       </div>
     </div>
